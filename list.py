3adc691cd0b7b5cacfa446088a022857b212c0e7
Miklos Quartus <qmi@localhost>
2014-11-04 20:18:18 +0100

    cae engineering suckers technical 'dumb' test solved in python in 10 mins

---
+++ list.py
@@ -0,0 +1,25 @@
+# CAE Engineering - szakmai teszt
+# Tue Nov  4 20:17:27 CET 2014
+# They wanted me to use an weird scripting language 'nasal' to solve it.
+# Here is it in Python :)
+# Adding 100 random elements of numbers to an array (list)
+# Calculate an average of the numbers in the array 
+
+import random
+
+#### fill array with 100 random numbers between 1-100
+a=[]
+i=0
+print "Adding elements to array."
+for i in range(0,100):
+	a.append(random.randint(1,50))
+	#print "Random element in array: %d" % a[i]
+print "%d of elements to array added." % len(a)
+
+#### calculate average
+average=0
+for i in a:
+	average=average+i
+
+result=average/100
+print "Average is: %d" % result
